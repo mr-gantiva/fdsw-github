0a9b435b0b61132d64c162667d3a203ef3860320
mr-gantiva <ivaganva@gmail.com>
2023-11-05 20:39:14 -0300

    Add fade animations with ScrollReveal && Js

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+ScrollReveal().reveal(".texto-banner", { delay: 1700 });
+ScrollReveal().reveal(".bg-mis-trabajos", { delay: 200 });
+ScrollReveal().reveal(".titulo-proyectos", { delay: 500 });
+ScrollReveal().reveal(".card", { interval: 300 });
